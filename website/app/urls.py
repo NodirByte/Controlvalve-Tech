985261fc8c5fb80ab91d03491e8115e0c6d81b59
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-06-15 17:30:10 +0500

    fix navbar bugs

--- website/app/urls.py
+++ website/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.base_view, name="base"),
-    path("company/", views.company_view, name="company")
+    path("company/", views.company_view, name="company"),
+    path("solutions/", views.solutions_view, name="solutions")
 ]
--- website/app/views.py
+++ website/app/views.py
@@ -2,14 +2,10 @@ from django.shortcuts import render
 from .models import SocialAccounts, Product, ProductType
 
 def base_view(request):
-    social_accounts = SocialAccounts.objects.first()
-    products = Product.objects.all()
-    product_types = ProductType.objects.all()
-    return render(request, 'layout/base.html', {
-        'social_accounts': social_accounts,
-        'products': products,
-        'product_types': product_types,
-   })
+    return render(request, 'main.html')
 
 def company_view(request):
     return render(request, 'company.html')
+
+def solutions_view(request):
+    return render(request, 'solutions.html')
