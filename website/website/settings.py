fee8ec768a044e0ac234946305d7182eb521a2c1
nodirbyte <nodirbyte@gmial.com>
2024-06-19 10:41:43 +0500

    Ready to deploy

--- website/website/settings.py
+++ website/website/settings.py
@@ -124,21 +124,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Define the static root
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Define the static root
 
+# Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # WhiteNoise settings
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
